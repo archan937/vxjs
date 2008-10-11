51c8aaec61650617e18e6b642c74c9d1a06896e7
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-11 20:58:52 +0000

    merged functions. set fourth arg to remove

--- event.js
+++ event.js
@@ -1,3 +1,1 @@
-function vE(e,t,f){if(e.attachEvent){e['e'+t+f]=f;e[t+f]=function(){e['e'+t+f](window.event)};e.attachEvent('on'+t,e[t+f])}else{e.addEventListener(t,f,false)}}
-
-function vEr(e,t,f){if(e.detachEvent){e.detachEvent('on'+t,e[t+f]);e[t+f]=null;}else{e.removeEventListener(t,f,false)}}
+function vE(e,t,f,r){if(r){if(e.attachEvent){if(e.detachEvent){e.detachEvent('on'+t,e[t+f]);e[t+f]=null;}else{e.removeEventListener(t,f,false)}}else{e['e'+t+f]=f;e[t+f]=function(){e['e'+t+f](window.event)};e.attachEvent('on'+t,e[t+f])}else{e.addEventListener(t,f,false)}}}
