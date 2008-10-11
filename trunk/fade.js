255dc5c92d102e6d7daf620cb49fb7d5975c2981
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-11 21:14:54 +0000

    user can change interval/frame

--- trunk/fade.js
+++ trunk/fade.js
@@ -1,3 +1,3 @@
-function vFc(h,s,e){vA(h,"opacity",s,e,"","",20,100);vA(h,"filter",s*100,e*100,"alpha(opacity=",")",20,100)}
-function vFi(h){vFc(h,0,1)}
-function vFo(h){vFc(h,1,0)}
+function vFc(h,s,e,f,i){vA(h,"opacity",s,e,"","",20,100);vA(h,"filter",s*100,e*100,"alpha(opacity=",")",f?f:20,i?i:100)}
+function vFi(h,f,i){vFc(h,0,1,f,i)}
+function vFo(h,f,i){vFc(h,1,0,f,i)}
