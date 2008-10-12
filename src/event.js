b41b4d1a2d5b7cf0508ad2bec2fccacc47cde4bc
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-12 20:06:43 +0000

    even lighter events

--- src/event.js
+++ src/event.js
@@ -1,13 +1,11 @@
 function vE(e,t,f,r){
   if(e.attachEvent){
-    if(r){
-      e.detachEvent('on'+t,e[t+f]);
-    }else{
+    r?e.detachEvent('on'+t,e[t+f]):r;
+    if(!r){
       e['e'+t+f]=f;
       e[t+f]=function(){e['e'+t+f](window.event)};
       e.attachEvent('on'+t,e[t+f]);
     }
-  }else{
-    r?e.removeEventListener(t,f,false):e.addEventListener(t,f,false)
-  }
+  }else
+    r?e.removeEventListener(t,f,false):e.addEventListener(t,f,false);
 }
