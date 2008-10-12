96f36120abb2d0de9dc5301271b0c47d867ccef0
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-12 01:36:11 +0000

    filled new directory structure

---
+++ src/event.js
@@ -0,0 +1,1 @@
+function vE(e,t,f,r){if(r){if(e.detachEvent){e.detachEvent('on'+t,e[t+f]);e[t+f]=null}else{e.removeEventListener(t,f,false)}}else{if(e.attachEvent){e['e'+t+f]=f;e[t+f]=function(){e['e'+t+f](window.event)};e.attachEvent('on'+t,e[t+f])}else{e.addEventListener(t,f,false)}}}
