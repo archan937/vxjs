a2a43bb0a727f4215786bc5f8ca15e651c04eb48
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-12 03:00:17 +0000

    shaved vS down 39b, updated packer to not overwrite if data's unchanged

--- packscript.php
+++ packscript.php
@@ -9,9 +9,11 @@ $content = '';
 foreach(glob('src/*.js') as $fn) {
 	$data = file_get_contents($fn);
 	$fn = explode('/',$fn,2);
-	file_put_contents('lib/'.$fn[1],trim(JSMin::minify($data)));
+	$prepared = trim(JSMin::minify($data));
+	if($prepared != @file_get_contents('lib/'.$fn[1])) file_put_contents('lib/'.$fn[1],$prepared);
 	$content .= "\n".$data;
 }
-file_put_contents('lib/vx.js',trim($header.JSMin::minify($content)));
+$prepared = trim($header.JSMin::minify($content));
+if($prepared != @file_get_contents('lib/'.$fn[1])) file_put_contents('lib/vx.js',$prepared);
 
 ?>
