2050a1335e50ce69212b57e6b35a19c5cdfc27be
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-12 03:00:17 +0000

    shaved vS down 39b, updated packer to not overwrite if data's unchanged

--- trunk/src/stringify.js
+++ trunk/src/stringify.js
@@ -1,1 +1,12 @@
-function vS(j) { if(!j) { if(j === undefined) return 'undefined'; if(j === null) return 'null'; return(j); } var c = j.constructor.toString(); var t = []; if(c.match(/object/i)) { for(x in j) t.push(x+':'+arguments.callee(j[x])); return '{'+t.join(',')+'}' } if(c.match(/array/i)) { for(x in j) t.push(arguments.callee(j[x])); return '['+t.join(',')+']' } if(c.match(/string/i)) return '\''+j.replace(/\'/g,'\\\'')+'\''; return j; }
+function vS(j) {
+	if(!j) return j==undefined?'undefined':(j===null?'null':j);
+	var f = arguments.callee, c = j.constructor.toString(), t = [];
+	if(c.match(/array/i)) {
+		for(x in j) t.push(f(j[x]));
+		return '['+t.join(',')+']'
+	} if(c.match(/object/i)) {
+		for(x in j) t.push(x+':'+f(j[x]));
+		return '{'+t.join(',')+'}'
+	} if(c.match(/string/i)) return "'"+j.replace(/\'/g,"\\'")+"'";
+	return j
+}
