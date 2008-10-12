bdf0290626a2650a93659bbeea3c38a1ac29b009
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-12 16:48:04 +0000

    smaller!

--- src/event.js
+++ src/event.js
@@ -1,1 +1,13 @@
-function vE(e,t,f,r){if(r){if(e.detachEvent){e.detachEvent('on'+t,e[t+f]);e[t+f]=null}else{e.removeEventListener(t,f,false)}}else{if(e.attachEvent){e['e'+t+f]=f;e[t+f]=function(){e['e'+t+f](window.event)};e.attachEvent('on'+t,e[t+f])}else{e.addEventListener(t,f,false)}}}
+function vE(e,t,f,r){
+  if(e.attachEvent){
+    if(r){
+      e.detachEvent('on'+t,e[t+f]);
+    }else{
+      e['e'+t+f]=f;
+      e[t+f]=function(){e['e'+t+f](window.event)};
+      e.attachEvent('on'+t,e[t+f]);
+    }
+  }else{
+    r?e.removeEventListener(t,f,false):e.addEventListener(t,f,false)
+  }
+}
