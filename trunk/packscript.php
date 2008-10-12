0909dd599b7230c68760ed92a309674bc690206d
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-12 00:42:56 +0000

    added stringify.js

--- trunk/packscript.php
+++ trunk/packscript.php
@@ -2,11 +2,14 @@
 
 require_once('jsmin.class.php');
 
+$header = <<<EOT
+/* vX Super Lightweight Ajax Library */
+EOT;
 $content = '';
 foreach(glob('*.js') as $fn) {
 	if($fn == 'vx.js') continue;
 	$content .= "\n".file_get_contents($fn);
 }
-file_put_contents('vx.js',trim(JSMin::minify($content)));
+file_put_contents('vx.js',trim($header.JSMin::minify($content)));
 
 ?>
