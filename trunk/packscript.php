c676deef1d7cd8c7a03b32a6078a773b139629e9
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-13 18:37:00 +0000

    packscript.php now generates modules.json

--- trunk/packscript.php
+++ trunk/packscript.php
@@ -12,16 +12,19 @@ function minify($s) {
 }
 
 $content = '';
+$lib = array();
 foreach(glob('src/vx.*.js') as $fn) {
 	$data = file_get_contents($fn);
 	$fn = explode('/',$fn,2);
 	$prepared = minify($data);
 	if($prepared != @file_get_contents('lib/'.$fn[1])) file_put_contents('lib/'.$fn[1],$prepared);
 	$content .= "\n".$data;
+	if(preg_match('/vx\.([^\.]+)\.js/',$fn[1],$m)) $lib[] = '\''.$m[1].'\'';
 }
 $prepared = minify(@file_get_contents('src/vx.js'));
 if($prepared != @file_get_contents('lib/vx.js')) file_put_contents('lib/vx.js',$prepared);
 $prepared = trim($header.minify($prepared.$content));
 if($prepared != @file_get_contents('lib/vx.all.js')) file_put_contents('lib/vx.all.js',$prepared);
-
+$prepared = '['.implode(',',$lib).']';
+if($prepared != @file_get_contents('modules.json')) file_put_contents('modules.json',$prepared);
 ?>
