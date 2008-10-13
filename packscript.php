73a62766025c99b99097896b01a40360eae52cc7
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-13 16:43:14 +0000

    set up namespace

--- packscript.php
+++ packscript.php
@@ -4,16 +4,22 @@ require_once('jsmin.class.php');
 
 $header = <<<EOT
 /* vX Super Lightweight Ajax Library */
+
 EOT;
+
+function minify($s) {
+	return str_replace(';}','}',trim(JSMin::minify($s)));
+}
+
 $content = '';
 foreach(glob('src/*.js') as $fn) {
 	$data = file_get_contents($fn);
 	$fn = explode('/',$fn,2);
-	$prepared = str_replace(";}","}",trim(JSMin::minify($data)));
+	$prepared = minify($data);
 	if($prepared != @file_get_contents('lib/'.$fn[1])) file_put_contents('lib/'.$fn[1],$prepared);
-	$content .= "\n".$data;
+	if($fn[1] != 'vx.js') $content .= "\n".$data;
 }
-$prepared = str_replace(";}","}",trim($header.JSMin::minify($content)));
-if($prepared != @file_get_contents('lib/'.$fn[1])) file_put_contents('lib/vx.js',$prepared);
+$prepared = trim($header.minify(@file_get_contents('lib/vx.js').$content));
+if($prepared != @file_get_contents('lib/vx.all.js')) file_put_contents('lib/vx.all.js',$prepared);
 
 ?>
