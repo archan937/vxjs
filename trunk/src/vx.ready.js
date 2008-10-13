ca50d737ace96a723e10a8012233de53f945ef50
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-13 18:10:42 +0000

    gave credit

--- trunk/src/vx.ready.js
+++ trunk/src/vx.ready.js
@@ -1,3 +1,4 @@
+//based on http://muffinresearch.co.uk/archives/2008/02/15/does-settimeout-solve-the-domcontentloaded-problem/
 _.R=function(f){
   if (/(?!.*?pati|.*?kit)^moz|ope/i.test(navigator.userAgent)){
     _.E(document,"DOMContentLoaded",f)
