efdc4a4e43f73648fd4c984197ebcab4988b9d03
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-13 16:43:45 +0000

    set up namespace

---
+++ src/vx.clone.js
@@ -0,0 +1,10 @@
+// depends on vS from stringify.js
+// based on code from http://oranlooney.com/functional-javascript/
+_('').C=function(j,c) {
+	// a copy is a duplicate of the original
+	if(c) return eval('('+_.S(j)+')');
+	// a clone is an empty object with a prototype reference to the original
+	function p() {}; // private constructor of this clone
+	p.prototype = j;
+    return new p()
+}
---
+++ src/vx.js
@@ -0,0 +1,7 @@
+window._ = function(n) {
+	var r = arguments.callee, p = n.split('.');
+	for(var i = 0; i < p.length && p[i]; i++) {
+		if(r[p[i]] == undefined) r[p[i]] = {};
+		r = r[p[i]]
+	} return r
+}
