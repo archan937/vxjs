b313c6e722abb48b959d42063c06676c912aa8af
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-13 19:06:19 +0000

    improved modules.json generation

--- trunk/src/vx.ready.js
+++ trunk/src/vx.ready.js
@@ -1,4 +1,5 @@
 //based on http://muffinresearch.co.uk/archives/2008/02/15/does-settimeout-solve-the-domcontentloaded-problem/
+/*[dep|event]*/
 _.R=function(f){
   if (/(?!.*?pati|.*?kit)^moz|ope/i.test(navigator.userAgent)){
     _.E(document,"DOMContentLoaded",f)
