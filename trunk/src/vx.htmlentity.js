5d93dbdd38107f599d8e9eae3e0cd11134541634
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-13 23:38:03 +0000

    shaved 8 bytes off _.H

--- trunk/src/vx.htmlentity.js
+++ trunk/src/vx.htmlentity.js
@@ -1,7 +1,7 @@
 /*[name|HTML Entity]*/
 /*[desc|encodes and decodes HTML entities]*/
 _.H=function(s,d) {
-	var e = document.createElement(d?'textarea':'div');
-	e.innerHTML = s;
-	return d?e.value:e.innerHTML;
+	var t = document.createElement('textarea');
+	t.innerHTML = s;
+	return d?t.value:t.innerHTML;
 }
