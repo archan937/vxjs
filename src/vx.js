8ad609e63160482b71e30e6e8ebe283756090d52
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-14 00:12:26 +0000

    guarded _ against repeat-inclusion

--- src/vx.js
+++ src/vx.js
@@ -1,4 +1,4 @@
-window._ = function(n) {
+var _ = _||function(n) {
 	var r = arguments.callee, p = n.split('.');
 	for(var i = 0; i < p.length && p[i]; i++) {
 		if(r[p[i]] == undefined) r[p[i]] = {};
