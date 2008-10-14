1a29cb6af252c976c33f146ed7ec2417fb92583f
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-14 20:11:43 +0000

    lighter ajax

--- src/vx.ajax.js
+++ src/vx.ajax.js
@@ -1,2 +1,12 @@
 /*[name|Ajax]*/
-_.X=function(u,f,p,x){x=(window.ActiveXObject)?new ActiveXObject("Microsoft.XMLHTTP"):new XMLHttpRequest();x.open(p?"POST":"GET",u,true);p?x.setRequestHeader("Content-type","application/x-www-form-urlencoded"):p;x.onreadystatechange=function(){if(x.readyState==4&&x.status==200) f?f(x.responseText,x):f};x.send(p)}
+
+_.X=function(u,f,p,x){
+  x=(window.ActiveXObject)?new ActiveXObject("Microsoft.XMLHTTP"):new XMLHttpRequest();
+  x.open(p?"POST":"GET",u,true);
+  p?x.setRequestHeader("Content-type","application/x-www-form-urlencoded"):p;
+  x.onreadystatechange=function(){
+    //if(x.readyState==4&&x.status==200) f?f(x.responseText,x):f
+    x.readyState==4?(f?f(x.responseText,x):0):0
+  };
+  x.send(p)
+}
