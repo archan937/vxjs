7d0170080d8979941b5900b2f27806cc35d68b5e
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-14 20:41:09 +0000

    did the !0 and !1 trick on clone/event as well

--- src/vx.clone.js
+++ src/vx.clone.js
@@ -5,7 +5,7 @@
 /*[ref|from http://oranlooney.com/functional-javascript/]*/
 _.C=function(j,c) {
 	// a copy is a duplicate of the original
-	if(c) return _.S(_.S(j),true);
+	if(c) return _.S(_.S(j),!0); //!0 = true
 	// a clone is an empty object with a prototype reference to the original
 	function p() {}; // private constructor of this clone
 	p.prototype = j;
