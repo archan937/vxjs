7f6cefe5b04c21b76cf0cf59a30cea5380589a5e
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-14 20:33:45 +0000

    Made vX Fade readable Now using single-quote (') convention

--- trunk/src/vx.ready.js
+++ trunk/src/vx.ready.js
@@ -1,9 +1,10 @@
 /*[dep|event]*/
 /*[name|Document onReady]*/
 /*[ref|based on http://muffinresearch.co.uk/archives/2008/02/15/does-settimeout-solve-the-domcontentloaded-problem/]*/
+
 _.R=function(f){
   if (/(?!.*?pati|.*?kit)^moz|ope/i.test(navigator.userAgent)){
-    _.E(document,"DOMContentLoaded",f)
+    _.E(document,'DOMContentLoaded',f)
   }else{
     setTimeout(f,0)
   }
