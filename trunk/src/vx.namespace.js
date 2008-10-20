7c8255c8bc1fbdbe4979b57c9da2917113429fdc
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-20 21:23:14 +0000

    fixed bug

--- trunk/src/vx.namespace.js
+++ trunk/src/vx.namespace.js
@@ -3,7 +3,7 @@
 _.N = function(n) {
 	var r = window, p = n.split('.');
 	for(var i = 0; i < p.length && p[i]; i++) {
-		if(!this.r[p[i]]) r[p[i]] = {};
+		if(r[p[i]] == undefined) r[p[i]] = {};
 		r = r[p[i]]
 	} return r
 }
