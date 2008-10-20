b67aea2f6a9333e0b4482019976e44e3c778e2fb
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-20 21:19:41 +0000

    _.N now operates under global window namespace

--- trunk/lib/vx.namespace.js
+++ trunk/lib/vx.namespace.js
@@ -1,1 +1,1 @@
-_.N=function(n){var r=_.N,p=n.split('.');for(var i=0;i<p.length&&p[i];i++){if(r[p[i]]==undefined)r[p[i]]={};r=r[p[i]]}return r}
+_.N=function(n){var r=window,p=n.split('.');for(var i=0;i<p.length&&p[i];i++){if(!this.r[p[i]])r[p[i]]={};r=r[p[i]]}return r}
