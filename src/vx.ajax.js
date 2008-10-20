781d81bc484b40f4f984788fa866396e4703dadd
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-20 23:31:55 +0000

    1 byte smaller!

--- src/vx.ajax.js
+++ src/vx.ajax.js
@@ -6,7 +6,8 @@ _.X=function(u,f,p,x){
   x.open(p?'POST':'GET',u,!0);
   p?x.setRequestHeader('Content-type','application/x-www-form-urlencoded'):0;
   x.onreadystatechange = function(){
-    x.readyState==4?f?f(x.responseText,x):f:0
+    //x.readyState==4?f?f(x.responseText,x):f:0
+    x.readyState==4&&f?f(x.responseText,x):f
   };
   x.send(p)
 }
