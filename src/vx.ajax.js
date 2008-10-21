9ceaa3d122b272e7344b3c822bdd0087d8e211df
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-21 00:25:33 +0000

    nutin

--- src/vx.ajax.js
+++ src/vx.ajax.js
@@ -1,12 +1,12 @@
 /*[name|Ajax]*/
 
 _.X=function(u,f,p,x){
-  //x=this.ActiveXObject?new ActiveXObject('Microsoft.XMLHTTP'):new XMLHttpRequest();
+//x=this.ActiveXObject?new ActiveXObject('Microsoft.XMLHTTP'):new XMLHttpRequest();
   x=new(this.ActiveXObject?ActiveXObject:XMLHttpRequest)('Microsoft.XMLHTTP');
   x.open(p?'POST':'GET',u,!0);
   p?x.setRequestHeader('Content-type','application/x-www-form-urlencoded'):0;
   x.onreadystatechange = function(){
-    //x.readyState==4?f?f(x.responseText,x):f:0
+  //x.readyState==4?f?f(x.responseText,x):f:0
     x.readyState==4&&f?f(x.responseText,x):f
   };
   x.send(p)
