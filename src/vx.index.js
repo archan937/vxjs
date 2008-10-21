664a3e7f6a4ea33da5d3cec3fd12e05c9ea65671
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-21 00:24:36 +0000

    docs

--- src/vx.index.js
+++ src/vx.index.js
@@ -2,7 +2,7 @@
 /*[name|Array IndexOf]*/
 /*[ref|based on http://snippets.dzone.com/posts/show/2437]*/
 
-_.I = function(v,a,i){
+_.I = function(/*Value*/v,/*Array*/a  /*[USELESS VAR*/,i){
   for(i = a.length; i-- && a[i] != v;);
   return i;
 }
