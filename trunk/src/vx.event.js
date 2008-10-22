6150b3456b8ef443d59336a07349c9dbe1751da0
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-22 23:16:02 +0000

    just a record of the pain and suffering i had to go through to make events for IE...

--- trunk/src/vx.event.js
+++ trunk/src/vx.event.js
@@ -1,6 +1,47 @@
 /*[name|Events]*/
 _.E=function(e,t,f,r){
   if(e.attachEvent){
+    e['e'+t+f]=f;
+    e[t+f]=function(){e['e'+t+f](window.event)};
+    r?e.detachEvent('on'+t,e[t+f]):e.attachEvent('on'+t,e[t+f]);
+  }else r?e.removeEventListener(t,f,!1):e.addEventListener(t,f,!1);
+}
+
+
+/*[name|Events]*/
+_.E=function(e,t,f,r){
+  if(e.attachEvent && r?e.detachEvent('on'+t,e[t+f]):!0){
+    e['e'+t+f]=f;
+    e[t+f]=function(){e['e'+t+f](window.event)};
+    e.attachEvent('on'+t,e[t+f]);
+  }else r?e.removeEventListener(t,f,!1):e.addEventListener(t,f,!1);
+}
+
+
+/*[name|Events]*/
+
+_.E=function(e,t,f,r){
+  if(e.attachEvent){
+    e['e'+t+f]=f;
+    e[t+f]=function(){e['e'+t+f](window.event)};
+    r?e.detachEvent('on'+t,e[t+f]):e.attachEvent('on'+t,e[t+f]);
+  }else r?e.removeEventListener(t,f,!1):e.addEventListener(t,f,!1);
+}
+
+
+/*[name|Events]*/
+
+_.E=function(e,t,f,r){
+  if(e.attachEvent){
+    r?0:e['e'+t+f]=f;
+    r?0:e[t+f]=function(){e['e'+t+f](window.event)};
+    r?e.detachEvent('on'+t,e[t+f]):e.attachEvent('on'+t,e[t+f]);
+  }else r?e.removeEventListener(t,f,!1):e.addEventListener(t,f,!1);
+}
+
+/*[name|Events]*/
+_.E=function(e,t,f,r){
+  if(e.attachEvent){
     //r?e.detachEvent('on'+t,e[t+f]):!0; //if no r then return true
     if(r?e.detachEvent('on'+t,e[t+f]):!0){ //should return true and fire if no r, detach if r
       e['e'+t+f]=f;
@@ -8,4 +49,20 @@ _.E=function(e,t,f,r){
       e.attachEvent('on'+t,e[t+f]);
     }
   }else r?e.removeEventListener(t,f,!1):e.addEventListener(t,f,!1);
+         r?(e.removeEventListener:e.addEventListener)(t,f,!1);
 }
+
+
+_.E=function(e,t,f,r){
+  if(e.attachEvent){
+    if(r?e.detachEvent('on'+t,e[t+f]):!0){
+      e['e'+t+f]=f;
+      e[t+f]=function(){e['e'+t+f](window.event)};
+      e.attachEvent('on'+t,e[t+f]);
+    }
+  }else r?e.removeEventListener(t,f,!1):e.addEventListener(t,f,!1);
+}
+
+
+
+(r?e.removeEventListener:e.addEventListener)(t,f,!1);
