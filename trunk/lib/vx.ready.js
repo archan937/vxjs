69625a8ff3e552381747546a1bbc8841f40958b2
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-22 22:12:10 +0000

    Smaller onReady

--- trunk/lib/vx.ready.js
+++ trunk/lib/vx.ready.js
@@ -1,1 +1,2 @@
-_.R=function(f){if(/(?!.*?ati|.*?kit)^moz|ope/i.test(navigator.userAgent)){_.E(document,'DOMContentLoaded',f)}else{setTimeout(f,0)}}
+_.R=function(f){if(/(?!.*?ati|.*?kit)^moz|ope/i.test(navigator.userAgent))
+_.E(document,'DOMContentLoaded',f);else setTimeout(f,0)}
