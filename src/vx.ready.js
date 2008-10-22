9c85db545e3c9e71251c80c5da07825ed805648f
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-22 22:17:21 +0000

    121 byte vx Readyness

--- src/vx.ready.js
+++ src/vx.ready.js
@@ -3,7 +3,7 @@
 /*[ref|based on http://muffinresearch.co.uk/archives/2008/02/15/does-settimeout-solve-the-domcontentloaded-problem/]*/
 
 _.R=function(f){
-  if (/(?!.*?ati|.*?kit)^moz|ope/i.test(navigator.userAgent))
-    _.E(document,'DOMContentLoaded',f);
-  else setTimeout(f,0)
+  (/(?!.*?ati|.*?kit)^moz|ope/i.test(navigator.userAgent))?
+    _.E(document,'DOMContentLoaded',f):
+    setTimeout(f,0)
 }
