a105a5f850478fe5e2a146bdd81a703f6f03c1e9
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-23 02:18:51 +0000

    docs

--- src/vx.index.js
+++ src/vx.index.js
@@ -6,11 +6,20 @@ _.I = function(/*Value*/v,/*Array*/a  /*[USELESS VAR*/,i){
   return i;
 }
 
-/*[summary|]*/
+/*[summary|Often, you need to check whether a string or object is in a specified array - something like with how strings have their indexOf property. ]*/
 /*[usage|
-
+`_.I(obj, array);`
+  * obj: object to search for
+  * array: the array to search in
+  * Note that it only gets the first occurance and ignores the following
 ]*/
 /*[example|
-
+{{{
+_.I("ham", ["bacon", "trees", "cows", "nuclear-pests"]);
+//-1
+var breakfast = ["bacon", "ham", "eggs", "javascript"]
+_.I("ham", breakfast);
+//1
+}}}
 ]*/
 /*[ref|based on http://snippets.dzone.com/posts/show/2437]*/
