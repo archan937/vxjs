e7e72414ea17a08cd478fe3250cb41ae1e27d1bd
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-23 01:28:13 +0000

    added documentation generator

--- trunk/packscript.php
+++ trunk/packscript.php
@@ -11,7 +11,7 @@ function minify($s) {
 	return str_replace(';}','}',trim(JSMin::minify($s)));
 }
 
-function parsecomments($s) {
+function parsemeta($s) {
 	if(!preg_match_all('/\/\*\[([^\|]+)\|([^\]]+)\]\*\//',$s,$matches,PREG_SET_ORDER)) return array();
 	$ret = array();
 	foreach($matches as $m) {
@@ -24,19 +24,22 @@ function parsecomments($s) {
 
 $content = '';
 $lib = array();
+$doc = '';
 foreach(glob('src/vx.*.js') as $fn) {
 	$data = file_get_contents($fn);
-	$metadata = parsecomments($data);
+	$metadata = parsemeta($data);
 	$fn = explode('/',$fn,2);
 	$prepared = minify($data);
 	if($prepared != @file_get_contents('lib/'.$fn[1])) file_put_contents('lib/'.$fn[1],$prepared);
 	$content .= "\n".$data;
-	if(preg_match('/vx\.([^\.]+)\.js/',$fn[1],$m)) $lib[$m[1]] = array(
-		'name'	=> @$metadata['name']?$metadata['name']:$m[1],
+	$modname = substr($fn[1],3,strlen($fn[1])-6);
+	$lib[$modname] = array(
+		'name'	=> @$metadata['name'][0]?$metadata['name'][0]:$modname,
 		'desc'	=> @$metadata['desc']?implode('. ',$metadata['desc']):'',
 		'note'	=> @$metadata['note']?implode('. ',$metadata['note']):'',
 		'dep'	=> @$metadata['dep']?explode(',',implode(',',$metadata['dep'])):array()
 	);
+	$doc .= '== '.trim(@$metadata['name'][0]?$metadata['name'][0]:$modname).': '.trim(@$metadata['desc']?implode("\n",$metadata['desc']):'').' =='."\n".'=== Usage ==='."\n".trim(@$metadata['usage']?implode("\n",$metadata['usage']):'')."\n".'=== Example ==='."\n".trim(@$metadata['summary']?implode("\n",$metadata['summary']):'')."\n\n";
 }
 $prepared = minify(@file_get_contents('src/vx.js'));
 if($prepared != @file_get_contents('lib/vx.js')) file_put_contents('lib/vx.js',$prepared);
@@ -44,4 +47,6 @@ $prepared = trim($header.minify($prepared.$content));
 if($prepared != @file_get_contents('lib/vx.all.js')) file_put_contents('lib/vx.all.js',$prepared);
 $prepared = json_encode($lib);
 if($prepared != @file_get_contents('modules.json')) file_put_contents('modules.json',$prepared);
+$prepared = trim($doc);
+if($prepared != @file_get_contents('doc.wiki')) file_put_contents('doc.wiki',$prepared);
 ?>
