d269f8916f0e852dcfe2ab6f0539f060a4e57256
inportb <inportb@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-23 01:40:18 +0000

    improved doc builder

--- trunk/src/vx.ajax.js
+++ trunk/src/vx.ajax.js
@@ -1,4 +1,23 @@
 /*[name|Ajax]*/
+/*[desc|AJAX Query]*/
+/*[summary|AJAX is a staple of the "Web 2.0" world. vX provides an extremely lightweight module to perform simple asynchronous GET/POST requests.]*/
+
+/*[usage|
+`_.X(url,callback[,query]);`
+  * url: the URL to query
+  * callback: function that is called when the query returns
+  * query: query string, sets POST mode when specified
+]*/
+
+/*[example|
+{{{
+// perform a GET request
+_.X("ajax.php?you=suck&howmuch=alot",function(response){alert(response);})
+
+// perform a POST request
+_.X("ajax.php",function(response){alert(response)},"you=suck&howmuch=alot")
+}}}
+]*/
 
 _.X=function(u,f,p,x){
 //x=this.ActiveXObject?new ActiveXObject('Microsoft.XMLHTTP'):new XMLHttpRequest();
