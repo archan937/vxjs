3b6e03e74181e142676c57698d5c4d6baa4378f7
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-23 02:18:44 +0000

    docs

--- trunk/src/vx.ready.js
+++ trunk/src/vx.ready.js
@@ -8,9 +8,10 @@ _.R=function(f){
     setTimeout(f,0)
 }
 
-/*[summary|]*/
+/*[summary| Many people use Window.onLoad to execute their scripts after the page has loaded. But, there is only 1 window.onload function you can set, and it happens after the images and etc are loaded. Document onReady allows you to execute javascript immediately when the document is ready for manipulation, before images are loaded and you can use it with multiple functions.]*/
 /*[usage|
-
+`_.R(function);`
+  * function: A javascript function to execute after the DOM is ready for manipulation
 ]*/
 /*[example|
 
