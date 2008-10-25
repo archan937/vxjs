c82149ff30fb3cb4ec9ea5db11210ebbe5ff0df0
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-25 17:26:30 +0000

    fade callbacks

--- trunk/src/vx.fade.js
+++ trunk/src/vx.fade.js
@@ -2,10 +2,10 @@
 /*[name|Fade]*/
 /*[desc|Simple Fade Animation]*/
 
-_.F=function(d,h,f,i){
+_.F=function(d,h,f,i,c){
   d=d=='in'; //direction
-  _.A(h,'opacity',d?0:1,d?1:0,'','',15,50); //all good browsers
-  _.A(h,'filter',d?0:100,d?100:0,'alpha(opacity=',')',f?f:15,i?i:50) //ie
+  _.A(h,'opacity',d?0:1,d?1:0,'','',15,50,c); //all good browsers
+  _.A(h,'filter',d?0:100,d?100:0,'alpha(opacity=',')',f?f:15,i?i:50,c) //ie
 }
 
 /*[summary|Fading is probably the most common and most useful effect. It makes sense most people don't like the complexity of the Core Animation function, and want a simpler function.]*/
