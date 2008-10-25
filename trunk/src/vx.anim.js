4b716738c095126000b7f9ae3eabd18b45f16ca1
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-25 16:35:42 +0000

    _.Ac -> _.O
    smaller animation core (x++>= -> ++x>)

--- trunk/src/vx.anim.js
+++ trunk/src/vx.anim.js
@@ -1,17 +1,17 @@
 /*[name|Animation]*/
 /*[desc|Basic Animation]*/
 
-_.Ac=function(v,n,c,u,y){
+_.O=function(v,n,c,u,y){
   u = 0;
   return y = setInterval(function(){
     c(u/v);
     //if(u++ >= v) clearInterval(y)
-    u++>=v?clearInterval(y):0
+    ++u>v?clearInterval(y):0
   },n);
 }
 
 _.A=function(h,p,s,e,r,x,f,i,b){
-  return _.Ac(f, i, function(a){
+  return _.O(f, i, function(a){
     (a==1 && b)?b():0;
     h.style[p]=r+(s+(e-s)*a)+x
   });
