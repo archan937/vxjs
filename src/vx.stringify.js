81f61bff8fed7b2b35e8727313d9230e1f71c0c1
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-25 16:31:46 +0000

    under 2kb again!

--- src/vx.stringify.js
+++ src/vx.stringify.js
@@ -5,17 +5,14 @@
 _.S=function(j,d,t) {
 	if(d) return eval('('+j+')');	
 	if(!j) return j+'';
-	if(!j||d){return }
 	t = [];
 	if(j.pop) {
 		for(x in j) t.push(_.S(j[x]));
 		j = '['+t.join(',')+']'
-	}else
-	if(typeof j=='object') {
+	}else if(typeof j=='object') {
 		for(x in j) t.push(x+':'+_.S(j[x]));
 		j = '{'+t.join(',')+'}'
-	}else
-	if(j.split) j = "'"+j.replace(/\'/g,"\\'")+"'";
+	}else if(j.split) j = "'"+j.replace(/\'/g,"\\'")+"'";
 	return j
 }
 
