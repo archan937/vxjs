4bb3b032489b296efba89725c2fd409027b6a206
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-25 16:29:22 +0000

    exactly 2kb

--- src/vx.stringify.js
+++ src/vx.stringify.js
@@ -1,19 +1,24 @@
 /*[name|Stringify]*/
 /*[desc|JSON Encoding]*/
 
-_.S=function(j,d) {
-	if(d) return eval('('+j+')');
+
+_.S=function(j,d,t) {
+	if(d) return eval('('+j+')');	
 	if(!j) return j+'';
-	var c = j.constructor+'', t = [];
-	if(c.match(/arr/i)) {
+	if(!j||d){return }
+	t = [];
+	if(j.pop) {
 		for(x in j) t.push(_.S(j[x]));
-		return '['+t.join(',')+']'
-	} if(c.match(/obj/i)) {
+		j = '['+t.join(',')+']'
+	}else
+	if(typeof j=='object') {
 		for(x in j) t.push(x+':'+_.S(j[x]));
-		return '{'+t.join(',')+'}'
-	} if(c.match(/str/i)) return "'"+j.replace(/\'/g,"\\'")+"'";
+		j = '{'+t.join(',')+'}'
+	}else
+	if(j.split) j = "'"+j.replace(/\'/g,"\\'")+"'";
 	return j
 }
+
 
 /*[summary|The JSON encoder serializes complex objects into JSON strings suitable for AJAX queries and unserializes JSON strings into objects.]*/
 /*[usage|
