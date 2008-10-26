15c03eb5ba22828b06c4b7eb9e97498ca2a98043
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-26 19:33:27 +0000

    queue docs

--- trunk/src/vx.queue.js
+++ trunk/src/vx.queue.js
@@ -1,7 +1,20 @@
+/*[name|Queue]*/
+/*[desc|Queue]*/
+
+
 //var lt=["_.F('in',document.body,0,0,fn)","_.F('out',document.body,0,0,fn)"];
 
-_.U = function(l,f){
-(f=function(){
-eval(l.splice(0,1)[0])
-})()
+_.U = function(l,f){ //l,f 
+  (f=function(){ //name function "f"
+    eval(l.splice(0,1)[0]) //cut off first in array, eval it
+  })() //execute function
 }
+
+
+/*[summary|]*/
+/*[usage|
+
+]*/
+/*[example|
+_.U(["_.F('in',document.body,0,0,f)","_.F('out',document.body,0,0,f)"])
+]*/
