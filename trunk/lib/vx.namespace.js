6dc52c13c6d0b2c73cde73419234edde53ee2f29
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-10-26 19:05:24 +0000

    smaller namespacing

--- trunk/lib/vx.namespace.js
+++ trunk/lib/vx.namespace.js
@@ -1,1 +1,4 @@
-_.N=function(n){var r=window,p=n.split('.');for(var i=0;i<p.length&&p[i];i++){if(r[p[i]]==undefined)r[p[i]]={};r=r[p[i]]}return r}
+_.N=function(n,p,r){p=n.split('.')
+r=window
+for(i in p){if(!r[p[i]])r[p[i]]={};r=r[p[i]]}
+return r}
