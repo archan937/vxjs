ebda9b32d06e5a4a45e19ad540d5abce85edccce
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-11-04 16:16:19 +0000

    new build system

--- trunk/packscript.php
+++ trunk/packscript.php
@@ -2,7 +2,8 @@
 
 require_once('jsmin.class.php');
 
-$header = "";
+$header = "var _=_?_:{";
+$footer = "\n}";
 
 function minify($s) {
 	return str_replace(';}','}',trim(JSMin::minify($s)));
@@ -22,13 +23,14 @@ function parsemeta($s) {
 $content = '';
 $lib = array();
 $doc = '';
+
 foreach(glob('src/vx.*.js') as $fn) {
 	$data = file_get_contents($fn);
 	$metadata = parsemeta($data);
 	$fn = explode('/',$fn,2);
 	$prepared = minify($data);
 	if($prepared != @file_get_contents('lib/'.$fn[1])) file_put_contents('lib/'.$fn[1],$prepared);
-	$content .= "\n".$data;
+	
 	$modname = substr($fn[1],3,strlen($fn[1])-6);
 	$lib[$modname] = array(
 		'name'	=> @$metadata['name'][0]?$metadata['name'][0]:$modname,
@@ -37,11 +39,20 @@ foreach(glob('src/vx.*.js') as $fn) {
 		'fn'	=> @$metadata['fn']?implode('. ',$metadata['fn']):'',
 		'dep'	=> @$metadata['dep']?explode(',',implode(',',$metadata['dep'])):array()
 	);
+	
+  $content .= "\n".$lib[$modname]['fn'].":".$prepared.",";
+		
 	$doc .= '== '.trim(@$metadata['name'][0]?$metadata['name'][0]:$modname).': '.trim(@$metadata['desc']?implode("\n",$metadata['desc']):'').' =='."\n".trim(@$metadata['summary']?implode("\n",$metadata['summary']):'')."\n".'=== Usage ==='."\n".trim(@$metadata['usage']?implode("\n",$metadata['usage']):'')."\n".'=== Example ==='."\n".trim(@$metadata['example']?implode("\n",$metadata['example']):'')."\n\n";
 }
 
+$content = substr($content,0,-1);
+
+$prepared = trim($header.$content.$footer);
+if($prepared != @file_get_contents('lib/vx.all.js')) file_put_contents('lib/vx.all.js',$prepared);
+
 $prepared = json_encode($lib);
 if($prepared != @file_get_contents('modules.json')) file_put_contents('modules.json',$prepared);
+
 $prepared = trim($doc);
 if($prepared != @file_get_contents('doc.wiki')) file_put_contents('doc.wiki',$prepared);
 ?>
