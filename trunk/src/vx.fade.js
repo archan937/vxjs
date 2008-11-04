69daa41a7ec649a405a01b1e69adc3f16b3a36ad
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-11-04 15:11:14 +0000

    fixed fading

--- trunk/src/vx.fade.js
+++ trunk/src/vx.fade.js
@@ -4,9 +4,11 @@
 
 _.F=function(d,h,f,i,c){
   d=d=='in'; //direction
-  _.A(h,'opacity',d?0:1,d?1:0,'','',15,50,c); //all good browsers
+  _.A(h,'opacity',d?0:1,d?1:0,'','',f?f:15,i?i:50,c); //all good browsers
   _.A(h,'filter',d?0:100,d?100:0,'alpha(opacity=',')',f?f:15,i?i:50,c) //ie
 }
+
+
 
 /*[summary|Fading is probably the most common and most useful effect. It makes sense most people don't like the complexity of the Core Animation function, and want a simpler function.]*/
 /*[usage|
