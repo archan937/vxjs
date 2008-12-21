0df0ab5120c3ab46c2ae5bd36c63dc52724f2e68
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2008-12-21 16:39:46 +0000

    vx.toarray now in core

---
+++ src/vx.array.js
@@ -0,0 +1,25 @@
+/*[name|Array]*/
+/*[desc|Convert iterables to real arrays]*/
+/*[fn|C]*/
+
+_.Y = function(a){
+  for(var b=a.length,c=[];b--;)c.push(a[b]);
+  return c
+}
+
+/*[summary|Accepts an array-like collection and returns an array equivalent]*/
+/*[usage|
+`_.Y(collection);`
+  * collection: anything with numberical iterators
+]*/
+/*[example|
+{{{
+// a bunch of dom nodes
+var original = document.body.childNodes
+// create a clone and override a property
+var array = _.Y(original)
+// array functions like splice, etc. 
+array.splice(1,1); //get rid of first object
+}}}
+]*/
+/*[ref|from http://www.prototypejs.org/api/utility/dollar-a]*/
