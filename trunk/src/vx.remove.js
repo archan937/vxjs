90a52dc97ad0c63367e52756d82b022c3a920123
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2009-01-12 21:52:04 +0000

    removed return

--- trunk/src/vx.remove.js
+++ trunk/src/vx.remove.js
@@ -4,8 +4,7 @@
 
 _.remove=_.V= function(e,o,x){
   x=_.I(e,o);
-  x>0?o.splice(x,1):0;
-  return o
+  x>0?o.splice(x,1):0
 }
 
 /*[summary|Removes an the first occurance of an element from an array. NOTE: MODIFIES ORIGINAL]*/
