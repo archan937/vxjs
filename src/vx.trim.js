533c0c0957464ae85798dd1ba934f82db61cb10b
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2009-01-12 22:44:13 +0000

    text trimming

---
+++ src/vx.trim.js
@@ -0,0 +1,19 @@
+/*[name|Trim String]*/
+/*[desc|Remove leading and ending whitespace]*/
+/*[fn|TM]*/
+
+_.trim = _.TM = function(t){
+  return t.replace(/^\s|\s+$/g,'')
+}
+
+/*[summary|Remove leading and ending whitespace]*/
+/*[usage|
+`_.TM(string);`
+  * string: a block of text
+]*/
+/*[example|
+{{{
+_.trim("   i am a blob of     text       ")
+//i am a blob of     text
+}}}
+]*/
