dff06ccee580431b40a09c54f7780d7da80b3ab5
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2009-01-12 21:50:30 +0000

    vx.remove!

---
+++ src/vx.remove.js
@@ -0,0 +1,26 @@
+/*[name|Array Remove]*/
+/*[desc|Remove items from arrays]*/
+/*[fn|V]*/
+
+_.remove=_.V= function(e,o,x){
+  x=_.I(e,o);
+  x>0?o.splice(x,1):0;
+  return o
+}
+
+/*[summary|Removes an the first occurance of an element from an array. NOTE: MODIFIES ORIGINAL]*/
+/*[usage|
+`_.V(element, array);`
+  * element: anything with numberical iterators
+  * array: anything with numberical iterators
+]*/
+/*[example|
+{{{
+// a bunch of dom nodes
+var original = document.body.childNodes
+// create a clone and override a property
+var array = _.Y(original)
+// array functions like splice, etc. 
+array.splice(1,1); //get rid of first object
+}}}
+]*/
