5fef82a8174cf3b8605a413d4d2e551eaf60f8ef
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2009-01-12 02:10:02 +0000

    added _.queue long name to queue

--- trunk/src/vx.queue.js
+++ trunk/src/vx.queue.js
@@ -4,7 +4,7 @@
 
 //var lt=["_.F('in',document.body,0,0,fn)","_.F('out',document.body,0,0,fn)"];
 
-_.U = function(l,n){ //l,f
+_.queue=_.U = function(l,n){ //l,f
   (n=function(){ //name function "f"
     eval(l.splice(0,1)[0]) //cut off first in array, eval it
   })(); //execute function
