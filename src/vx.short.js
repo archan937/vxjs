3d0093299130a023e72f54033123459f52469dc6
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2009-01-12 22:14:28 +0000

    vX short!

---
+++ src/vx.short.js
@@ -0,0 +1,15 @@
+/*[name|vX Shortcuts]*/
+/*[desc|Used mostly internally]*/
+/*[fn|d]*/
+
+_.d=document
+
+/*[summary|lets vX be smaller]*/
+/*[usage|
+document = _.d
+]*/
+/*[example|
+{{{
+document = _.d
+}}}
+]*/
