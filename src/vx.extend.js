b7df24d030dbffdee7ada3c14b43ac6113db4fcd
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2009-06-26 21:07:31 +0000

    docs for extend

--- src/vx.extend.js
+++ src/vx.extend.js
@@ -7,10 +7,23 @@ _.extend=_.T = function(o,a,y){
   return o
 }
 
-/*[summary|]*/
+/*[summary|Sometimes you need to extend an object with new attributes.]*/
 /*[usage|
+`_.T(original, new);`
 
+  * original: the first JS object
+  * new: the extendee whose attributes will be put in the new object
 ]*/
 /*[example|
-
+  {{{
+    _.extend({
+      "hello": "world",
+      "internet": "LOLCAT"
+    },
+    {
+      "name": "toni avonni",
+      "job": "Cheese salesman"
+    });
+    //{"hello": "world", "internet": "LOLCAT", "name":"toni avonni","job": "cheese salesman"}
+  }}}
 ]*/
