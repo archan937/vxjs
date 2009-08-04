67f6118e260f07b1660c5b8eae15703eb304a5e1
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2009-08-04 03:19:38 +0000

    added new anim arg

--- src/vx.anim.js
+++ src/vx.anim.js
@@ -2,22 +2,23 @@
 /*[desc|Basic Animation]*/
 /*[fn|A]*/
 
-_.fx=_.A=function(v,n,c,u,y){
+_.fx=_.A=function(v,n,c,f,u,y){
   u = 0;
-  return y = setInterval(function(){
-    c(u/v);
-    //if(u++ >= v) clearInterval(y)
-    ++u>v?clearInterval(y):0
-  },n);
+  (function(){
+    c(u/v); //execute the callback
+    u++<v? //check if reached max frames
+    setTimeout(arguments.callee,n): //if not then continue
+    (f?f():0) //or else end and callback
+  })() //run first loop
 }
 
-
-/*[summary|Here is a low level complicated yet flexible linear animation function. It is not reccomended]*/
+/*[summary|Here is a low level complicated yet flexible linear animation function. It is not recomended for normal use]*/
 /*[usage|
-`_.A(frames, interval, callback);`
+`_.A(frames, interval, callback, finishcallback);`
   * frames: number of blocks to split animation into
   * interval: numer of milliseconds between frames
   * callback: function called at every frame with the argument a number between 0 and 1
+  * finishcallback: a callback for when the animation is over
 ]*/
 /*[example|
 {{{
--- src/vx.fade.js
+++ src/vx.fade.js
@@ -3,7 +3,7 @@
 /*[desc|Simple Fade Animation]*/
 /*[fn|F]*/
 
-_.fade=_.F=function(d,h,f,i){
+_.fade=_.F=function(d,h,e,f,i){
   d=d=='in';
   _.A(f?f:15,i?i:50,function(a){
     a=(d?0:1)+(d?1:-1)*a;
@@ -15,12 +15,13 @@ _.fade=_.F=function(d,h,f,i){
 
 /*[summary|Fading is probably the most common and most useful effect. It makes sense most people don't like the complexity of the Core Animation function, and want a simpler function.]*/
 /*[usage|
-`_.F(DIRECTION IN/OUT, HTML ELEMENT[, FRAMES, INTERVAL]);`
+`_.F(DIRECTION IN/OUT, HTML ELEMENT[, END CALLBACK, FRAMES, INTERVAL]);`
 
   * Direction is lowercase, either "in" or "out"
   * Frames/interval are optional
   * Frames is number of frames in animation
   * Interval is the frame delay
+  * end callback is functino executed at end
   * note: anything not 'in' is treated as 'out'
 ]*/
 /*[example|
