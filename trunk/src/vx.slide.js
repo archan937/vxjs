203a4fc2cd5d8fe8129a1c425be045a8b6a05b6e
antimatter15 <antimatter15@e182cd4a-95b4-11dd-a3c2-4d3bc4297ea2>
2009-08-04 03:34:42 +0000

    more cool stuff

--- trunk/src/vx.slide.js
+++ trunk/src/vx.slide.js
@@ -3,18 +3,21 @@
 /*[fn|slide]*/
 /*[dep|anim]*/
 
-_.slide = function(d,e,o,q){
-q = e.clientHeight;
-_.A(15,10,function(a){
-a=(d?0:1)+(d?1:-1)*a;
-e.style.height = (a*q)+'px';
-},o)
+_.slide = function(d,e,o,f,i,q){
+  q = e.clientHeight;
+  _.A(f?f:15,i?i:10,function(a){
+    a=(d?0:1)+(d?1:-1)*a;
+    e.style.height = (a*q)+'px';
+  },o)
 }
 
 /*[summary|The query encoder builds an HTTP query string out of a Javascript object; this simple serialization scheme prepares data for sending to the server.]*/
 /*[usage|
-`_.slide(direction,element);`
+`_.slide(direction,element[, finishcallback, frames, interval]);`
   * direction: to expand or to contract
   * element: the element to animate
+  * finishcallback: function to call when done
+  * frames: number of frames in animation
+  * interval: milliseconds between frames
 ]*/
 
